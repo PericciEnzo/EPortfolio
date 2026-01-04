52be2ee05d4983c22b1a536857611964ccf17d4f
Dateg0000 <enzopericci@gmail.com>
2026-01-04 17:51:39 +0100

    modif global

--- assets/app.js
+++ assets/app.js
@@ -38,3 +38,28 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 });
+
+// Navigation active
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.nav-link');
+    
+    window.addEventListener('scroll', () => {
+        let current = '';
+        
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (window.pageYOffset >= sectionTop - 100) {
+                current = section.getAttribute('id');
+            }
+        });
+        
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            if (link.getAttribute('href') === `#${current}`) {
+                link.classList.add('active');
+            }
+        });
+    });
+});
--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,11 +8,12 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class BlogController extends AbstractController
 {
-    #[Route(' ', name: 'app_blog')]
+    #[Route('/', name: 'app_blog')]
     public function index(): Response
     {
         return $this->render('blog/index.html.twig');
     }
+
     #[Route('/mentions-legales', name: 'app_legal')]
     public function legal(): Response
     {
@@ -23,5 +24,29 @@ final class BlogController extends AbstractController
     public function privacy(): Response
     {
         return $this->render('footer/privacy.html.twig');
+    }
+
+    #[Route('/cv', name: 'app_cv')]
+    public function cv(): Response
+    {
+        return $this->render('blog/cv.html.twig');
+    }
+
+    #[Route('/competences', name: 'app_competences')]
+    public function competences(): Response
+    {
+        return $this->render('blog/competences.html.twig');
+    }
+
+    #[Route('/projets', name: 'app_projets')]
+    public function projets(): Response
+    {
+        return $this->render('blog/projets.html.twig');
+    }
+
+    #[Route('/a-propos', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('blog/about.html.twig');
     }
 }
